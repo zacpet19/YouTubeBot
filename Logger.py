eec15c5b02417f78846d1942407ba8c8f78944f0
Ethan <ethdew19@gmail.com>
2022-12-12 00:05:25 -0600

    logger

--- Logger.py
+++ Logger.py
@@ -1,16 +1,40 @@
+from datetime import datetime
+
+
 class Logger:
 
+
+    def __init__(self):
+        self.name = "Epic logger"
+        self.logFile = open("logs.txt","w", encoding="utf-8")
+    
     def debug(self, string: str):
-        print(string)
+        text = f"[{self.getTime()}][DEBUG] {string}"
+        self.logFile.write(text + '\n')
+        print(text)
 
     def info(self, string: str):
-        print(string)
+        text = f"[{self.getTime()}][INFO] {string}"
+        print(text)
+        self.logFile.write(text + '\n')
 
     def warn(self, string: str):
-        print(string)
+        text = f"[{self.getTime()}][WARN] {string}"
+        print(text)
+        self.logFile.write(text + '\n')
 
     def error(self, string: str):
-        print(string)
+        text = f"[{self.getTime()}][ERROR] {string}"
+        print(text)
+        self.logFile.write(text + '\n')
+
+    def getTime(self):
+        now = datetime.now()
+        return now.strftime("%d/%m/%Y %H:%M:%S")
+
+
+
+    
 
 
 
