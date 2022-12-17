5d824a79ab1431129b990326968d1d584ef2373d
zacpet19 <zachpeterson54@gmail.com>
2022-12-16 20:25:19 -0600

    Finished tests for TextToSpeech method and started main

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+import redditScraper
+from TextToSpeech import TextToSpeech
 def main():
-    print("hello world")
-# Press the green button in the gutter to run the script.
+    comments = redditScraper.getTopPostComments("csmajors")
+    TextToSpeech.textToSpeech(comments)
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
     main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- tests/TextToSpeechTest.py
+++ tests/TextToSpeechTest.py
@@ -1,7 +1,22 @@
 import sys
+import os
 sys.path.append('..')
 
 from TextToSpeech import TextToSpeech
+
+def TexttoSpeechTest():
+    comment = [["hello", "goodbye", "Hey there"], ["Hello, GoodBye", "Hey there"]]
+    textToSpeech = TextToSpeech.textToSpeech(comment)
+    assert os.path.exists("./audio")
+    assert os.path.exists("./audio/1.mp3")
+    assert os.path.exists("./audio/2.mp3")
+    os.remove("./audio/1.mp3")
+    os.remove("./audio/2.mp3")
+    os.rmdir("./audio")
+    print("tests completed")
+
+TexttoSpeechTest()
+
 
 
 
