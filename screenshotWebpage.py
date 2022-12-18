f072bb93441d8dafd58df4e1580dea07f915252b
Ethan <ethdew19@gmail.com>
2022-12-17 18:04:46 -0600

    changes for updated selenium

--- screenshotWebpage.py
+++ screenshotWebpage.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
 from PIL import Image
 import io
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome("")
-
+ser = Service()
+op = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=ser, options=op)
 url = "https://www.reddit.com/r/csMajors/comments/zn6k7r/recruiter_call_wasted_me_whole_30_mins_and_tells/"
 
 
 driver.get(url)
-element = driver.find_element_by_css_selector("[data-testid='post-container']")
+element = driver.find_element(By.CSS_SELECTOR, "[data-testid='post-container']")
 image = element.screenshot_as_png
 
 imageStream = io.BytesIO(image)
