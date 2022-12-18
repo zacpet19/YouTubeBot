0a97c95652560d3f717810fadf15ff52bb22100f
zacpet19 <zachpeterson54@gmail.com>
2022-12-17 18:53:56 -0600

    added error checking for screentshot method and turned it into object

--- screenshotWebpage.py
+++ screenshotWebpage.py
@@ -3,19 +3,30 @@ from PIL import Image
 import io
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+class ScreenShot:
 
-ser = Service()
-op = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=ser, options=op)
-url = "https://www.reddit.com/r/csMajors/comments/zn6k7r/recruiter_call_wasted_me_whole_30_mins_and_tells/"
+    def __init__(self, path):
+        try:
+            ser = Service(path)
+            self.driver = webdriver.Chrome(service=ser, options=webdriver.ChromeOptions())
+        except Exception as e:
+            print("Error: Incorrect path to Chrome Driver.")
+            raise e
 
+    def takeScreenShot(self, url):
+        try:
+            self.driver.get(url)
+        except Exception as e:
+            print("Error: Failed to access URL")
+            raise e
+        try:
+            element = self.driver.find_element(By.CSS_SELECTOR, "[data-testid='post-container']")
+        except Exception as e:
+            print("Error: failed to find CSS element on webpage")
+            raise e
+        image = element.screenshot_as_png
+        imageStream = io.BytesIO(image)
+        im = Image.open(imageStream)
+        im.save("a.png")
 
-driver.get(url)
-element = driver.find_element(By.CSS_SELECTOR, "[data-testid='post-container']")
-image = element.screenshot_as_png
-
-imageStream = io.BytesIO(image)
-im = Image.open(imageStream)
-im.save("a.png")
-
-driver.quit()
+        self.driver.quit()
