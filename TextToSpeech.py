29bfb7efdf961079e49af5544bb7a77ef0183f5c
zacpet19 <zachpeterson54@gmail.com>
2022-12-21 15:57:02 -0600

    Created mergeAudioFiles and updated makeAudioFileSameLength methods

--- TextToSpeech.py
+++ TextToSpeech.py
@@ -1,6 +1,7 @@
 from gtts import gTTS
 from moviepy.editor import AudioFileClip
 from moviepy.audio.fx.audio_loop import *
+from moviepy.editor import CompositeAudioClip
 import os 
 
 class TextToSpeech:
@@ -19,23 +20,33 @@ class TextToSpeech:
 
     @staticmethod
     def makeAudioFileSameLength(clipPath : str, clipToChangePath : str):
-        """This will take in filepaths to two audio files with the first file being the one that will set the lenght
-        for the other file. This might need to be run in a try block because it throws a (OSError: [WinError 6] The
-        handle is invalid) even if it successfully makes a new mp3 and the reason for this cannot be found at this
-        time"""
+        """This will take in filepaths to two audio files with the first file being the one that will set the length
+        for the other file."""
+        if not os.path.exists("./audio"):
+            os.makedirs("./audio")
         clip = AudioFileClip(clipPath)
         clipToChange = AudioFileClip(clipToChangePath)
         if clip.duration < clipToChange.duration:
-            clipToChange = clipToChange.subclip(0, clip.duration - 1)
-            clipToChange.write_audiofile("modMusic.mp3")
-            print("music shortened")
+            clipToChangeSubclip = clipToChange.subclip(0, clip.duration - 1)
+            clipToChangeSubclip.write_audiofile("audio/modMusic.mp3")
+            clipToChangeSubclip.close()
         else:
-            #succesfully loops the audio and creates a new file but errors out for some reason
-            clipToChange = audio_loop(clipToChange, duration=clip.duration - 1)
-            clipToChange.write_audiofile("modMusic.mp3")
-            print("musiclooped")
+            clipToChangeLooped = audio_loop(clipToChange, duration=clip.duration - 1)
+            clipToChangeLooped.write_audiofile("audio/modMusic.mp3")
+            clipToChangeLooped.close()
+        clip.close()
+        clipToChange.close()
 
-
+    @staticmethod
+    def mergeAudioFiles(clipsToMerge : list):
+        """Takes in a list of audio clip objects and then merges them all together and saves the new clip into memory.
+        They will all start at the same time so if they vary in length the longest one will continue to play to
+         completion after the others have stopped"""
+        if not os.path.exists("./audio"):
+            os.makedirs("./audio")
+        mergedAudio = CompositeAudioClip(clipsToMerge)
+        mergedAudio.write_audiofile("audio/finalAudio.mp3", fps=44100)
+        mergedAudio.close()
 
 
 
