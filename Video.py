53fb4c1dd7c430268c76795dbcd8fe5b4fe8f3a0
zacpet19 <zachpeterson54@gmail.com>
2022-12-22 16:28:31 -0600

    Changed the name of setVideoDurationAndRemoveAudio to formatVideoForYoutubeShort and made it also set the size resolution of a video. Then added the methods resizeImageToEvenDimensions and resizeVideoClip

--- Video.py
+++ Video.py
@@ -2,12 +2,13 @@ from moviepy.video.io.VideoFileClip import VideoFileClip
 from moviepy.editor import concatenate_videoclips
 from moviepy.video.VideoClip import ImageClip
 import moviepy.video.fx.all as vfx
+from PIL import Image
 import os
 
 
 class VideoMethods:
     @staticmethod
-    def setVideoDurationAndRemoveAudio(videoPath : str, duration : int, startCut=0):
+    def formatVideoForYoutubeShort(videoPath : str, duration : int, startCut=0):
         """Takes in a filepath to a mp4 file and then makes it the input duration. If the video is longer than the
         duration it cuts the end of the video to the given duration and if the video is shorter than the given
         duration it loops it until it is long enough. The startCut parameter allows for choosing when to start the
@@ -15,7 +16,9 @@ class VideoMethods:
         if not os.path.exists("./video"):
             os.makedirs("./video")
         try:
-            clip = VideoFileClip(videoPath)
+            #might want to find a different method of resizing the video because depending on the initial quality of
+            #the video it could be really ugly
+            clip = VideoFileClip(videoPath, target_resolution=(1920, 1080))
         except Exception as e:
             print("Error: Could not find file.")
             return False
@@ -43,6 +46,23 @@ class VideoMethods:
         clip.close()
 
     @staticmethod
+    def resizeImageToEvenDimensions(imagePath : str):
+        try:
+            image = Image.open(imagePath)
+        except Exception as e:
+            print("File not found")
+            raise e
+        (width, height) = image.size
+        if height % 2 != 0:
+            height += 1
+        if width % 2 != 0:
+            width += 1
+        reSizedImage = image.resize((width, height))
+        reSizedImage.save("reSizedImage.png")
+        reSizedImage.close()
+        image.close()
+
+    @staticmethod
     def createImageVideo(imagePath : str, duration : int):
         """IMPORTANT:Image passed in must have even dimensions or the video made will be black!!!!"""
         if not os.path.exists("./video"):
@@ -54,6 +74,14 @@ class VideoMethods:
             return False
         clip.write_videofile("video/imageVideo.mp4", fps=24)
         clip.close()
+
+    @staticmethod
+    def resizeVideoClip(clipPath : str, height : int, width : int):
+        clip = VideoFileClip(clipPath, target_resolution=(height, width))
+        clip.write_videofile("video/resizedVideo.mp4")
+        clip.close()
+
+
 
 
 
