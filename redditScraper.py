fb125c97dfecff124d379743684b95e7b1b9b3b7
Ethan <ethdew19@gmail.com>
2022-12-28 01:40:13 -0600

    RedditScraper: store urls to avoid duplicate posts

--- redditScraper.py
+++ redditScraper.py
@@ -6,10 +6,29 @@ import praw
 class RedditScraper:
     def __init__(self,client_id,client_secret, user_agent):
         self.reddit = praw.Reddit(client_id=client_id,client_secret=client_secret,user_agent=user_agent)
+        self.shouldCensor = False
+        self.pastUrls = set()
+        try:
+            f = open("visitedRedditPages.txt", "r")
+            lines = f.readlines()
+            for line in lines:
+                self.pastUrls.add(line)
+        except Exception:
+            print("Failed to open visitedRedditPages.txt, duplicate posts may be used")
+        #Check if bannedWordList exists
+        try:
+            f = open("bannedWordList.txt", "r")
+            f.close()
+            self.shouldCensor = True
+        except Exception:
+            print("Failed to open bannedWordList.txt, reddit text will not be censored")
+
+        
 
     def getTopPostComments(self, subreddit : str) -> tuple[list[list[str]], list[str]]:
         """getTopPostComments takes in the name of the subreddit and then returns a tuple that contains a 2d array of posts
         and comments as well as a list of urls"""
+
         topComments = []
         urlArray = []
         for post in self.reddit.subreddit(subreddit).hot(limit=20):
@@ -17,7 +36,10 @@ class RedditScraper:
                 break
             if (post.stickied or post.url.endswith(('jpg', 'jpeg', 'png', 'gif'))):
                 continue
+            if (post.url in self.pastUrls):
+                continue
             parsedBody = self.parsePostBody(post.selftext)
+            #If body is too long we move to next post
             if(parsedBody == "error"):
                 continue
             tempList = []
@@ -30,6 +52,7 @@ class RedditScraper:
                 if(top_level_comment.stickied):
                     continue
                 parsed = self.parseComments(top_level_comment.body)
+                #Ignore comments that are too long
                 if parsed == "error":
                     continue
                 tempList.append(parsed)
@@ -39,27 +62,39 @@ class RedditScraper:
                     break
             topComments.append(tempList)
         print(topComments)
+        #Document visited urls to avoid duplicate 
+        #TODO : check if \n is needed
+        f = open("visitedRedditPages.txt", "a")
+        for url in urlArray:
+            f.write(url + '\n')
+        f.close()
+
         return (topComments, urlArray)
 
-    #TODO: for both parsers make sure to remove \n and \ before apostrphes maybe
-    @staticmethod
-    def parseComments(comment : str) -> str:
+
+    def parseComments(self,comment : str) -> str:
         if(len(comment) > 1000):
             return "error"
+        if not self.shouldCensor:
+            return comment
         f = open("bannedWordList.txt","r")
         lines = f.readlines()
         for line in lines:
             comment = comment.replace(line,"*" * len(line))
+        f.close()
         return comment
 
-    @staticmethod
-    def parsePostBody(body : str) -> str:
+
+    def parsePostBody(self,body : str) -> str:
         if(len(body) > 2500):
             return "error"
+        if not self.shouldCensor:
+            return body
         f = open("bannedWordList.txt", "r")
         lines = f.readlines()
         for line in lines:
             body = body.replace(line, "*" * len(line))
+        f.close()
         return body
 
 
